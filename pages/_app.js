61048639c0eece0811e885435519bfb856a59cdf
henrik Sissener <henrik@godtlevert.no>
2021-04-11 11:41:48 +0200

    Added gtm tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 
 import { RecoilRoot } from "recoil";
 
 import ThemeSwitcher from "../components/themeSwitcher";
 
-import ReactGA from "react-ga";
-
-ReactGA.initialize("G-TDN290C5H6");
+import TagManager from "react-gtm-module";
 
 268497789;
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (TagManager) {
+      TagManager.initialize({ gtmId: "GTM-K885GDV" });
+    }
+  }, []);
   return (
     <RecoilRoot>
       <ThemeSwitcher>
