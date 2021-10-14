c09ecd7bd12c1055d900664920f8e4595650775d
henrik Sissener <henrik@godtlevert.no>
2021-10-14 22:35:17 +0200

    removed unused code

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,4 +1,5 @@
-import React, { useMemo, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import React from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Physics, usePlane, useSphere } from "@react-three/cannon";
 import { IoArrowBackSharp } from "react-icons/io5";
@@ -9,75 +10,74 @@ import Meta from "../../components/Meta/Meta";
 
 import Link from "next/link";
 
-// function randomIntFromInterval(min, max) {
-//   // min and max included
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
+// A physical sphere tied to mouse coordinates without visual representation
+function Mouse() {
+  const { viewport } = useThree();
+  const [, api] = useSphere(() => ({ type: "Kinematic", args: [6] }));
+  return useFrame((state) =>
+    api.position.set(
+      (state.mouse.x * viewport.width) / 2,
+      (state.mouse.y * viewport.height) / 2,
+      7
+    )
+  );
+}
+
+// A physical plane without visual representation
+function Plane({ ...props }) {
+  usePlane(() => ({ ...props }));
+  return null;
+}
+
+// Creates a crate that catches the spheres
+function Borders() {
+  const { viewport } = useThree();
+  return (
+    <>
+      <Plane
+        position={[0, -viewport.height / 2, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      />
+      <Plane
+        position={[-viewport.width / 2 - 1, 0, 0]}
+        rotation={[0, Math.PI / 2, 0]}
+      />
+      <Plane
+        position={[viewport.width / 2 + 1, 0, 0]}
+        rotation={[0, -Math.PI / 2, 0]}
+      />
+      <Plane position={[0, 0, 0]} rotation={[0, 0, 0]} />
+      <Plane position={[0, 0, 12]} rotation={[0, -Math.PI, 0]} />
+    </>
+  );
+}
+
+// Spheres falling down
+function InstancedSpheres({ count = 200 }) {
+  const { viewport } = useThree();
+  const [ref] = useSphere(() => ({
+    mass: 100,
+    position: [4 - Math.random() * 8, viewport.height, 0, 0],
+    args: [1],
+  }));
+  return (
+    <instancedMesh
+      ref={ref}
+      castShadow
+      receiveShadow
+      args={[null, null, count]}
+    >
+      <sphereBufferGeometry args={[1, 32, 32]} />
+      <meshLambertMaterial color="#ff7b00" />
+    </instancedMesh>
+  );
+}
+
+InstancedSpheres.propTypes = {
+  count: PropTypes.number,
+};
 
 export default function Gallery() {
-  // A physical sphere tied to mouse coordinates without visual representation
-  function Mouse() {
-    const { viewport } = useThree();
-    const [, api] = useSphere(() => ({ type: "Kinematic", args: [6] }));
-    return useFrame((state) =>
-      api.position.set(
-        (state.mouse.x * viewport.width) / 2,
-        (state.mouse.y * viewport.height) / 2,
-        7
-      )
-    );
-  }
-
-  // A physical plane without visual representation
-  function Plane({ color, ...props }) {
-    usePlane(() => ({ ...props }));
-    return null;
-  }
-
-  // Creates a crate that catches the spheres
-  function Borders() {
-    const { viewport } = useThree();
-    return (
-      <>
-        <Plane
-          position={[0, -viewport.height / 2, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-        />
-        <Plane
-          position={[-viewport.width / 2 - 1, 0, 0]}
-          rotation={[0, Math.PI / 2, 0]}
-        />
-        <Plane
-          position={[viewport.width / 2 + 1, 0, 0]}
-          rotation={[0, -Math.PI / 2, 0]}
-        />
-        <Plane position={[0, 0, 0]} rotation={[0, 0, 0]} />
-        <Plane position={[0, 0, 12]} rotation={[0, -Math.PI, 0]} />
-      </>
-    );
-  }
-
-  // Spheres falling down
-  function InstancedSpheres({ count = 200 }) {
-    const { viewport } = useThree();
-    const [ref] = useSphere((index) => ({
-      mass: 100,
-      position: [4 - Math.random() * 8, viewport.height, 0, 0],
-      args: [1],
-    }));
-    return (
-      <instancedMesh
-        ref={ref}
-        castShadow
-        receiveShadow
-        args={[null, null, count]}
-      >
-        <sphereBufferGeometry args={[1, 32, 32]} />
-        <meshLambertMaterial color="#ff7b00" />
-      </instancedMesh>
-    );
-  }
-
   return (
     <>
       <Meta />
