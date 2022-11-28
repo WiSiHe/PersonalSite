6c31c335100148d8ffe762b38425383c7183e354
Henrik WIlhelm Sissener <hws902@gmail.com>
2022-11-28 13:15:22 +0100

    removed redundant react import

--- services/sanity/sanityClient.js
+++ services/sanity/sanityClient.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 
-import imageUrlBuilder from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url"
 
-import config from "./config";
+import config from "./config"
 
 const builder = imageUrlBuilder({
   projectId: config.projectId,
-  dataset: config.dataset,
-});
+  dataset: config.dataset
+})
 
 // Then we like to make a simple function like this that gives the
 // builder an image and returns the builder for you to specify additional
 // parameters:
 export function urlFor(source) {
-  return builder.image(source);
+  return builder.image(source)
 }
