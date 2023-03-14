5617248f78b2e2fa56014cf355be922488d1c333
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-03-14 20:32:18 +0100

    sitemap stuff

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   siteUrl: "https://wisihe.no",
   generateRobotsTxt: true, // (optional)
+  exclude: ["/test", "/test/*"],
   // ...other options
 }
--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  siteUrl: "https://wisihe.no",
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
