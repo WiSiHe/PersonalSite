169f2c47023cb91286d7c3664b2c7322a759ddad
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-05-15 09:07:00 +0200

    Added new links to footer

--- components/templates/Footer/Footer.tsx
+++ components/templates/Footer/Footer.tsx
@@ -17,7 +17,14 @@ const Footer = () => {
       className="flex items-center justify-center w-full gap-4 p-10 text-white bg-dark"
     >
       <SocialLinks />
-      <Link href="/test">Test Page</Link>
+      <Link
+        href="https://643465dda2acbd1c279ba003-vnmadehkun.chromatic.com/?path=/docs/example-introduction--docs"
+        target="_blank"
+      >
+        Design system
+      </Link>
+      <Link href="/style">Styleguide</Link>
+      <Link href="/test">Test Pages</Link>
     </motion.footer>
   )
 }
