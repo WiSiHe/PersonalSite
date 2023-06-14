8ef554852481d406f0ad646302b54b2ba8986c38
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-06-14 15:48:22 +0200

    improved search results

--- components/atoms/SanitySearch/SanitySearch.tsx
+++ components/atoms/SanitySearch/SanitySearch.tsx
@@ -1,16 +1,17 @@
 "use client"
+import { iSanityImage } from "lib/models/objects/sanityImage"
 import client, { urlFor } from "lib/sanity"
+import Image from "next/image"
 import { useEffect, useState } from "react"
 
 interface Painting {
   title: string
   name: string
-  image: any
+  image: iSanityImage
 }
 
-// use single quotes for sanity queries
-// disable eslint for this line
-const types = ["painting", "tag", "video", "project"]
+//  prettier-ignore
+const types = ['painting', 'tag', 'video', 'project']
 
 const SanitySearch = () => {
   const [paintings, setPaintings] = useState<Painting[]>([])
@@ -18,22 +19,16 @@ const SanitySearch = () => {
 
   async function search(query: string) {
     if (!query) return []
-    console.log("searching for", query)
-    const modifiedQuery = `${query}*`
+    const mQ = `${query}*`
+    const limit = 10
     const results = await client.fetch(
-      `*[_type in ['tag', 'video', 'painting', 'project'] && title match $modifiedQuery || name match $modifiedQuery ]{title, image}`,
-      { modifiedQuery }
+      `*[_type in $types && title match $mQ || name match $mQ ][0...$limit]{title, name, image}`,
+      { mQ, types, limit }
     )
     setPaintings(results)
   }
 
   //   *[_type in $__types && (_id match $t0 || _type match $t0 || title match $t0 || description match $t0 || ogImage.title match $t0 || pt::text(description[]) match $t0 || format match $t0 || slug.current match $t0 || slug.source match $t0 || seoDescription match $t0 || video match $t0 || redbubbleUrl match $t0 || society6Url match $t0 || artstationUrl match $t0 || inPrintUrl match $t0 || instagramUrl match $t0 || twitterUrl match $t0 || paintedAt match $t0 || name match $t0 || videoUrl match $t0 || status match $t0 || projectStart match $t0 || projectEnd match $t0 || connectedPage.current match $t0 || connectedPage.source match $t0 || pt::text(content[]) match $t0)]
-
-  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault()
-  //   const query = e.currentTarget.elements[0].value
-  //   await search(query)
-  // }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.currentTarget.value)
@@ -63,13 +58,16 @@ const SanitySearch = () => {
           return (
             <div key={i} className="col-span-3 p-4 bg-white aspect-square">
               <h2>{title || name}</h2>
-              {}
+
               {image && (
-                <img
-                  src={urlFor(painting.image).width(200).url()}
-                  alt={painting.title}
-                  className="object-cover w-full h-full"
-                />
+                <div className="relative aspect-square">
+                  <Image
+                    src={urlFor(painting.image).width(200).url()}
+                    alt={painting.title}
+                    fill
+                    className="object-cover w-full h-full"
+                  />
+                </div>
               )}
             </div>
           )
