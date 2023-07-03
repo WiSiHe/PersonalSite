03d216ee10b4e0a85d9376fede6004ef8595c9d7
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-07-03 22:15:21 +0200

    1 more fix for revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -32,22 +32,41 @@
 //     return res.status(500).send("Error while revalidating")
 //   }
 // }
-import { isValidRequest, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
 
-const secret = process.env.SANITY_WEBHOOK_SECRET
+// import { isValidRequest, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
 
-export default async function handler(req, res) {
-  const signature = request.headers[SIGNATURE_HEADER_NAME]
-  const body = await readBody(req) // Read the body into a string
-  if (!isValidSignature(body, signature, secret)) {
-    res.status(401).json({ success: false, message: "Invalid signature" })
-    return
-  }
+// const secret = process.env.SANITY_WEBHOOK_SECRET
 
-  const jsonBody = JSON.parse(body)
-  doSomeMagicWithPayload(jsonBody)
-  res.json({ success: true })
-}
+// export default async function handler(req, res) {
+//   const signature = request.headers[SIGNATURE_HEADER_NAME]
+//   const body = await readBody(req) // Read the body into a string
+//   if (!isValidSignature(body, signature, secret)) {
+//     res.status(401).json({ success: false, message: "Invalid signature" })
+//     return
+//   }
+
+//   const jsonBody = JSON.parse(body)
+//   doSomeMagicWithPayload(jsonBody)
+//   res.json({ success: true })
+// }
+
+// // Next.js will by default parse the body, which can lead to invalid signatures
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// }
+
+// async function readBody(readable) {
+//   const chunks = []
+//   for await (const chunk of readable) {
+//     chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk)
+//   }
+//   return Buffer.concat(chunks).toString("utf8")
+// }
+
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
+import client from "lib/sanity"
 
 // Next.js will by default parse the body, which can lead to invalid signatures
 export const config = {
@@ -56,10 +75,69 @@ export const config = {
   },
 }
 
+const AUTHOR_UPDATED_QUERY = /* groq */ `
+  *[_type == "painting" && _id == $id] {
+    "slug": *[_type == "post" && references(^._id)].slug.current
+  }["slug"][]`
+const POST_UPDATED_QUERY = /* groq */ `*[_type == "post" && _id == $id].slug.current`
+const PAINTING = /* groq */ `*[_type == "painting" && _id == $id].slug.current`
+
+const getQueryForType = (type) => {
+  switch (type) {
+    case "author":
+      return AUTHOR_UPDATED_QUERY
+    case "post":
+      return POST_UPDATED_QUERY
+    case "painting":
+      return PAINTING
+    default:
+      throw new TypeError(`Unknown type: ${type}`)
+  }
+}
+
+const log = (msg, error) =>
+  console[error ? "error" : "log"](`[revalidate] ${msg}`)
+
 async function readBody(readable) {
   const chunks = []
   for await (const chunk of readable) {
     chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk)
   }
   return Buffer.concat(chunks).toString("utf8")
+}
+
+export default async function revalidate(req, res) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME]
+  const body = await readBody(req) // Read the body into a string
+  if (!isValidSignature(body, signature, process.env.SANITY_WEBHOOK_SECRET)) {
+    const invalidSignature = "Invalid signature"
+    log(invalidSignature, true)
+    res.status(401).json({ success: false, message: invalidSignature })
+    return
+  }
+
+  const jsonBody = JSON.parse(body)
+  const { _id: id, _type } = jsonBody
+  if (typeof id !== "string" || !id) {
+    const invalidId = "Invalid _id"
+    log(invalidId, true)
+    return res.status(400).json({ message: invalidId })
+  }
+
+  log(`Querying post slug for _id '${id}', type '${_type}' ..`)
+  const slug = await client.fetch(getQueryForType(_type), { id })
+  const slugs = (Array.isArray(slug) ? slug : [slug]).map(
+    (_slug) => `/painting/${_slug}`
+  )
+  const staleRoutes = ["/", ...slugs]
+
+  try {
+    await Promise.all(staleRoutes.map((route) => res.revalidate(route)))
+    const updatedRoutes = `Updated routes: ${staleRoutes.join(", ")}`
+    log(updatedRoutes)
+    return res.status(200).json({ message: updatedRoutes })
+  } catch (err) {
+    log(err.message, true)
+    return res.status(500).json({ message: err.message })
+  }
 }
