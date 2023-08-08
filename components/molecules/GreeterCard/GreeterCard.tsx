df8fbf777ff9adf3a700c2cee30822a6e6a47bac
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-08-08 09:03:57 +0200

    removing spline for now

--- components/molecules/GreeterCard/GreeterCard.tsx
+++ components/molecules/GreeterCard/GreeterCard.tsx
@@ -43,48 +43,57 @@ const desktopVariant = {
 
 const GreeterCard = () => {
   return (
-    <section className="relative flex flex-col justify-center h-full gap-4 bg-white rounded-lg overflow-clip drop-shadow-lg">
-      <div
+    <>
+      {/* <Script
+        type="module"
+        src="https://unpkg.com/@splinetool/viewer/build/spline-viewer.js"
+      /> */}
+      <section className="relative flex flex-col justify-center h-full gap-4 bg-white rounded-lg overflow-clip drop-shadow-lg">
+        {/* <div
         dangerouslySetInnerHTML={{
           __html:
             '<spline-viewer url="https://prod.spline.design/NJPbTHKSakGvCHjI/scene.splinecode" events-target="global"></spline-viewer>',
         }}
         className="absolute inset-0 w-full h-full"
-      />
-      <div className="z-10 flex flex-col items-start h-full px-4 pt-4 pb-16 text-white lg:justify-center lg:p-4 mix-blend-difference">
-        <h1 className="text-2xl xl:text-5xl">
-          <strong>Henrik Wilhelm Sissener</strong>
-        </h1>
-        <ul className="flex flex-col gap-2 mt-4 text-base xl:text-2xl">
-          <motion.li
-            className="flex items-center gap-2 p-1"
-            whileHover="hover1"
-            initial="initial"
-            animate="animate"
-          >
-            <motion.div variants={desktopVariant}>
-              <HiOutlineDesktopComputer />
-            </motion.div>
-            <span>Senior Front-end developer</span>
-          </motion.li>
-          <motion.li
-            className="flex items-center gap-2 p-1"
-            whileHover="hover2"
-          >
-            <motion.div variants={brushVariant}>
-              <BsFillBrushFill />
-            </motion.div>
-            <span>Digital artist</span>
-          </motion.li>
-          <motion.li whileHover="hover" className="flex items-center gap-2 p-1">
-            <motion.div variants={gameIconVariant}>
-              <BiGame />
-            </motion.div>
-            <span>Game developer</span>
-          </motion.li>
-        </ul>
-      </div>
-    </section>
+      /> */}
+        <div className="z-10 flex flex-col items-start h-full px-4 pt-4 pb-16 text-white lg:justify-center lg:p-4 mix-blend-difference">
+          <h1 className="text-2xl xl:text-5xl">
+            <strong>Henrik Wilhelm Sissener</strong>
+          </h1>
+          <ul className="flex flex-col gap-2 mt-4 text-base xl:text-2xl">
+            <motion.li
+              className="flex items-center gap-2 p-1"
+              whileHover="hover1"
+              initial="initial"
+              animate="animate"
+            >
+              <motion.div variants={desktopVariant}>
+                <HiOutlineDesktopComputer />
+              </motion.div>
+              <span>Senior Front-end developer</span>
+            </motion.li>
+            <motion.li
+              className="flex items-center gap-2 p-1"
+              whileHover="hover2"
+            >
+              <motion.div variants={brushVariant}>
+                <BsFillBrushFill />
+              </motion.div>
+              <span>Digital artist</span>
+            </motion.li>
+            <motion.li
+              whileHover="hover"
+              className="flex items-center gap-2 p-1"
+            >
+              <motion.div variants={gameIconVariant}>
+                <BiGame />
+              </motion.div>
+              <span>Game developer</span>
+            </motion.li>
+          </ul>
+        </div>
+      </section>
+    </>
   )
 }
 
--- components/organisms/PaintingGrid/PaintingGrid.tsx
+++ components/organisms/PaintingGrid/PaintingGrid.tsx
@@ -5,7 +5,6 @@ import { AnimatePresence, motion } from "framer-motion"
 import { iSanityPainting } from "lib/models/objects/sanityPainting"
 import { useCombinedStore } from "lib/store"
 import { useRouter, useSearchParams } from "next/navigation"
-import Script from "next/script"
 import { useEffect, useMemo, useState } from "react"
 import { isEmptyArray, isNotEmptyArray } from "utils/array"
 import { sortPaintings } from "utils/painting"
@@ -125,57 +124,51 @@ const PaintingGrid = ({
   }, [paintings])
 
   return (
-    <>
-      <Script
-        type="module"
-        src="https://unpkg.com/@splinetool/viewer/build/spline-viewer.js"
-      />
-      <section className="grid items-start w-full h-full grid-cols-12 gap-4 mb-10 grid-flow-dense">
-        {!hasFilters && (
-          <div className="flex flex-col h-full gap-4 col-span-full xl:col-span-6 aspect-auto xl:aspect-auto">
-            <GreeterCard />
-          </div>
-        )}
+    <section className="grid items-start w-full h-full grid-cols-12 gap-4 mb-10 grid-flow-dense">
+      {!hasFilters && (
+        <div className="flex flex-col h-full gap-4 col-span-full xl:col-span-6 aspect-auto xl:aspect-auto">
+          <GreeterCard />
+        </div>
+      )}
 
-        <AnimatePresence>
-          {!isEmptyArray(filterPaintingsV2) ? (
-            filterPaintingsV2
-              .slice(0, paintingsSlice)
-              .map((painting) => (
-                <Painting
-                  paintingData={painting}
-                  key={painting._id}
-                  storybook={isStorybook}
-                />
-              ))
-          ) : (
-            <motion.div
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ type: "spring" }}
-              key="no-paintings-found"
-              className={cn(
-                "flex flex-col items-center justify-center h-full gap-2 p-4 rounded-lg col-span-full ring  ring-primary",
-                hasFilters ? "bg-primary/10" : "xl:col-span-6"
-              )}
+      <AnimatePresence>
+        {!isEmptyArray(filterPaintingsV2) ? (
+          filterPaintingsV2
+            .slice(0, paintingsSlice)
+            .map((painting) => (
+              <Painting
+                paintingData={painting}
+                key={painting._id}
+                storybook={isStorybook}
+              />
+            ))
+        ) : (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "spring" }}
+            key="no-paintings-found"
+            className={cn(
+              "flex flex-col items-center justify-center h-full gap-2 p-4 rounded-lg col-span-full ring  ring-primary",
+              hasFilters ? "bg-primary/10" : "xl:col-span-6"
+            )}
+          >
+            <h2 className="text-2xl text-center">
+              No paintings found with the selected filters
+            </h2>
+            <p className="text-center">
+              Try removing some filters to see more paintings
+            </p>
+            <button
+              onClick={handleClearFilter}
+              className="px-4 py-3 text-white rounded-lg bg-primary hover:bg-primary/90"
             >
-              <h2 className="text-2xl text-center">
-                No paintings found with the selected filters
-              </h2>
-              <p className="text-center">
-                Try removing some filters to see more paintings
-              </p>
-              <button
-                onClick={handleClearFilter}
-                className="px-4 py-3 text-white rounded-lg bg-primary hover:bg-primary/90"
-              >
-                Clear Filters
-              </button>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </section>
-    </>
+              Clear Filters
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </section>
   )
 }
 
