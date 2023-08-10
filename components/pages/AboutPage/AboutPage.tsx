7966c2ffd82f1449eba2381454a55262716c9c61
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-08-10 12:52:23 +0200

    padding

--- components/pages/AboutPage/AboutPage.tsx
+++ components/pages/AboutPage/AboutPage.tsx
@@ -89,7 +89,7 @@ const AboutPage = ({ paintings = [] }: AboutPageProps) => {
     <>
       <section
         key="hero"
-        className="relative w-full flex gap-8 flex-col xl:grid items-center xl:grid-cols-12 min-h-[600px] px-8 xl:px-4 py-10 xl:gap-10 xl:items-center h-fit xl:min-h-[100dvh overflow-clip"
+        className="relative w-full flex gap-8 flex-col xl:grid items-center xl:grid-cols-12 min-h-[600px] px-4 py-10 xl:gap-10 xl:items-center h-fit xl:min-h-[100dvh overflow-clip"
       >
         <Image
           src={currentBackground ? currentBackground.image : ""}
@@ -138,35 +138,6 @@ const AboutPage = ({ paintings = [] }: AboutPageProps) => {
         </section>
       </section>
 
-      <section className="relative text-white pt-14 xl:pt-24">
-        <div className="relative w-full px-4 text-dark xl:px-10">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.8, y: 100 }}
-            whileInView={{ opacity: 1, scale: 1, y: 0 }}
-            transition={{ type: "spring", duration: 1.5 }}
-          >
-            <h2 className="pb-2">Paintings</h2>
-            <p className="max-w-2xl">
-              As a Digital Artist, my passion leans towards creating stylized
-              portraits, ethereal landscapes, and artwork that transports you
-              into the cosmos. I also occasionally taking up commissioned work.{" "}
-              <Link href="/paintings" className="underline">
-                Check out my gallery
-              </Link>
-            </p>
-          </motion.div>
-        </div>
-      </section>
-
-      <div className="w-full pt-8 xl:pl-6">
-        <CarouselStatic />
-      </div>
-
-      <div className="flex flex-col items-center justify-center w-full gap-4 py-10">
-        <strong>Like what you see?</strong>
-        <LinkButton href="/paintings">More paintings</LinkButton>
-      </div>
-
       <section className="flex flex-col items-center justify-center w-full min-h-screen gap-4 p-4 py-24 text-white bg-dark">
         <motion.div
           initial={{ opacity: 0, scale: 0.8, y: 100 }}
@@ -199,6 +170,34 @@ const AboutPage = ({ paintings = [] }: AboutPageProps) => {
           </p>
         </motion.div>
       </section>
+      <section className="relative text-white pt-14 xl:pt-24">
+        <div className="relative w-full px-4 text-dark xl:px-10">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8, y: 100 }}
+            whileInView={{ opacity: 1, scale: 1, y: 0 }}
+            transition={{ type: "spring", duration: 1.5 }}
+          >
+            <h2 className="pb-2">Paintings</h2>
+            <p className="max-w-2xl">
+              As a Digital Artist, my passion leans towards creating stylized
+              portraits, ethereal landscapes, and artwork that transports you
+              into the cosmos. I also occasionally taking up commissioned work.{" "}
+              <Link href="/paintings" className="underline">
+                Check out my gallery
+              </Link>
+            </p>
+          </motion.div>
+        </div>
+      </section>
+
+      <div className="w-full pt-8 xl:pl-6">
+        <CarouselStatic />
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full gap-4 py-10">
+        <strong>Like what you see?</strong>
+        <LinkButton href="/paintings">More paintings</LinkButton>
+      </div>
 
       {/* <section className="relative p-4 bg-dark overflow-clip">
         <div className="grid h-full max-w-screen-xl grid-cols-12 gap-4 mx-auto my-auto items xl:aspect-video">
