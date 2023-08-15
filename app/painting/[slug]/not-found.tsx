682b109bcbc21ed6634eddd7e13d1fd9dfb87c96
Henrik WIlhelm Sissener <hws902@gmail.com>
2023-08-15 09:35:11 +0200

    Removed redundant code

--- app/painting/[slug]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import ErrorPage from "components/pages/ErrorPage"
-
-export default function NotFound() {
-  return (
-    <ErrorPage title="Sorry I could not find the painting you were looking for!" />
-  )
-}
